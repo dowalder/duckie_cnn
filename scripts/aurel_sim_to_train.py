2ff23b0f7c4cedda0c51231a0b6a4f65193e03d6
Dominik Walder <dominikwalder@gmx.ch>
2018-07-04 13:48:58 +0200

    made script aurel_sim_to_train.py useful with cmd line args

--- scripts/aurel_sim_to_train.py
+++ scripts/aurel_sim_to_train.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+import argparse
 import pathlib
+import random
 import csv
 import PIL.Image
 
 
-def load_sim_folder(sim_dir: pathlib.Path, tgt_dir: pathlib.Path) -> None:
+def load_sim_folder(sim_dir: pathlib.Path, tgt_dir: pathlib.Path, test_percentage=0.3) -> None:
     data_csv = sim_dir / "collected_data" / "processed_data.csv"
     images = sim_dir / "collected_data" / "images"
+    test_dir = tgt_dir / "test"
+    train_dir = tgt_dir / "train"
 
     if not data_csv.is_file():
         raise RuntimeError("Could not find the required file: {}".format(data_csv))
@@ -24,8 +28,12 @@ def load_sim_folder(sim_dir: pathlib.Path, tgt_dir: pathlib.Path) -> None:
             if not img_src.is_file():
                 raise RuntimeError("Could not find image at: {}".format(img_src))
 
-            img_tgt = tgt_dir / img_src.name
-            lbl_tgt = tgt_dir / "{}.txt".format(img_src.stem)
+            if random.random() < test_percentage:
+                img_tgt = test_dir / img_src.name
+                lbl_tgt = test_dir / "{}.txt".format(img_src.stem)
+            else:
+                img_tgt = train_dir / img_src.name
+                lbl_tgt = train_dir / "{}.txt".format(img_src.stem)
 
             img = PIL.Image.open(img_src.as_posix())
             img.resize((160, 120)).save(img_tgt.as_posix())
@@ -33,9 +41,26 @@ def load_sim_folder(sim_dir: pathlib.Path, tgt_dir: pathlib.Path) -> None:
 
 
 def main():
-    img_dir = pathlib.Path("/home/dominik/dataspace/images/aurel_sim/sim_0")
-    tgt_dir = pathlib.Path("/home/dominik/tmp/sim_0")
-    load_sim_folder(img_dir, tgt_dir)
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--src_dir", "-s", required=True, help="directory containing sim dirs")
+    parser.add_argument("--tgt_dir", "-t", required=True, help="where to store extracted data")
+
+    args = parser.parse_args()
+
+    src_dir = pathlib.Path(args.src_dir)
+    tgt_dir = pathlib.Path(args.tgt_dir)
+
+    if not src_dir.is_dir():
+        raise ValueError("--src_dir must be a valid directory, but it not: ".format(src_dir))
+    tgt_dir.mkdir(parents=True, exist_ok=True)
+    (tgt_dir / "test").mkdir(exist_ok=True)
+    (tgt_dir / "train").mkdir(exist_ok=True)
+
+    for folder in src_dir.iterdir():
+        if not folder.is_dir():
+            continue
+        load_sim_folder(folder, tgt_dir)
 
 
 if __name__ == "__main__":
